ab3d4936cd7698836a0820167b82e2e3588e233e
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-05-02 11:49:55 -0300

    atualziação 01

---
+++ ms.js
@@ -0,0 +1,72 @@
+//MENU RESPONSIVO
+let menuResponsivo = document.querySelector('.menu-site');
+document.querySelector('#menu').onclick =() =>{
+    menuResponsivo.classList.toggle('active');
+
+    
+
+    
+
+}
+
+
+
+window.onscroll = () =>{
+    carrinho.classList.remove('active');
+    login.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider",{
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    grapCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    }
+
+})
+
+
+
+var swiper = new Swiper(".menu-slider", {
+    grapCursor:true,
+    loop:true,
+    autoHeight:true,
+    centeredSlides:true,
+    spaceBetwwen:20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+  
+  });
+  
+  //JANELA MODAL KAYRTON//
+  let verModalCorpo = document.querySelector(".menu-modal-container");
+  let verModalBox = verModalCorpo.querySelector(".menu-modal");
+
+  document.querySelectorAll(".menu .box").forEach(menu =>{
+    menu.onclick = () =>{
+        verModalCorpo.style.display = 'flex';
+        let nome = menu.getAttribute('data-name');
+
+
+        verModalBox.forEach(visualizar =>{
+
+            let chamada = visualizar.getAttribute('data-target');
+            if(nome == chamada){
+                visualizar.classList.add('active');
+            }
+
+        });
+
+    };
+
+  });
+
+
